7d6f2211541b5c2d935fbe7c77b5f31691b33abe
Vincent DEFERT <20.100@defert.com>
2022-08-11 22:06:11 +0200

    Added basic support for cooperative multitasking

--- include/uni-STC/uni-STC.h
+++ include/uni-STC/uni-STC.h
@@ -15,13 +15,13 @@
 #ifdef __SDCC
 	#define CRITICAL __critical
 	#define REENTRANT __reentrant
-	#define USE_BANK(registerBank) __using(registerBank)
+	#define USING(registerBank) __using(registerBank)
 	// With SDCC, we always optimise (generally for size), so no problem.
 	#define INLINE inline
 #else
 	#define CRITICAL
 	#define REENTRANT
-	#define USE_BANK(registerBank)
+	#define USING(registerBank)
 	#define INTERRUPT_USING(name, vector, regnum) void name()
 	// GCC & CLANG never inline functions unless optimisation is requested, 
 	// which is not the case by default, so we need to insist a little bit.
@@ -32,5 +32,21 @@
 	#define __xdata
 	#define __code
 #endif // __SDCC
+
+#ifdef COOPERATIVE_MULTITASKING
+	/**
+	 * Simple cooperative multitasking support: long-running tasks
+	 * should call yield() whenever it's possible and safe for them to
+	 * stop their work - which they should do if yield() returns true.
+	 * 
+	 * The yield() function must be implemented in the C module where
+	 * the super loop is defined.
+	 */
+	bool yield();
+#else
+	// This trick allows the same code to work with or without
+	// cooperative multitasking support.
+	#define yield() false
+#endif
 
 #endif // _UNISTC_UNISTC_H

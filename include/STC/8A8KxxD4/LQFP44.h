3c2f68e0d2aa060a3197e30fc9346ed4c56df5cb
Vincent DEFERT <20.100@defert.com>
2023-06-04 11:45:41 +0200

    Added missing packages for STC8A

---
+++ include/STC/8A8KxxD4/LQFP44.h
@@ -0,0 +1,21 @@
+#ifndef _STC8A8KXXD4_LQFP44_H
+#define _STC8A8KXXD4_LQFP44_H
+
+#define MCU_FAMILY 8
+#define MCU_SERIES 'A'
+#define MCU_PINS 44
+#define MCU_HAS_COMPARATOR
+#define COMPARATOR_4P2N
+#define MCU_HAS_MDU
+#define MCU_HAS_DMA
+#define MCU_HAS_LCM
+#define NB_UARTS 4
+#define PWM_GROUPS 1
+#define PWM_CHANNELS 8
+#define ADC_CHANNELS 15
+#define SPI_HAS_HIGH_SPEED
+#define NB_TIMERS 5
+
+#include <uni-STC/stcmcu.h>
+
+#endif // _STC8A8KXXD4_LQFP44_H
